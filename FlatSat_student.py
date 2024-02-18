50e940030c0f2dc665c087c3b573866c12857854
sgcodehack <114374534+sgcodehack@users.noreply.github.com>
2024-02-17 16:13:31 -0800

    Add files via upload

---
+++ FlatSat_student.py
@@ -0,0 +1,101 @@
+"""
+The Python code you will write for this module should read
+acceleration data from the IMU. When a reading comes in that surpasses
+an acceleration threshold (indicating a shake), your Pi should pause,
+trigger the camera to take a picture, then save the image with a
+descriptive filename. You may use GitHub to upload your images automatically,
+but for this activity it is not required.
+
+The provided functions are only for reference, you do not need to use them. 
+You will need to complete the take_photo() function and configure the VARIABLES section
+"""
+
+#AUTHOR: Sachit Gonur
+#DATE: 02/03/2024
+
+#import libraries
+import time
+import sys
+sys.path.append("/home/pi/MIT-CubeSat/FlatSat_student.py")
+print(sys.path)
+import board
+from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
+from adafruit_lis3mdl import LIS3MDL
+from git import Repo
+from picamera2 import Picamera2, Preview
+
+#VARIABLES
+THRESHOLD = 5      #Any desired value from the accelerometer
+REPO_PATH = "https://github.com/sgcodehack/MIT-CubeSat.git"     #Your github repo path: ex. /home/pi/FlatSatChallenge
+FOLDER_PATH = "/Images"   #Your image folder path in your GitHub repo: ex. /Images
+
+#imu and camera initialization
+i2c = board.I2C()
+accel_gyro = LSM6DS(i2c)
+mag = LIS3MDL(i2c)
+picam2 = Picamera2()
+
+
+def git_push():
+    """
+    This function is complete. Stages, commits, and pushes new images to your GitHub repo.
+    """
+    try:
+        repo = Repo(REPO_PATH)
+        origin = repo.remote('origin')
+        print('added remote')
+        origin.pull()
+        print('pulled changes')
+        repo.git.add(REPO_PATH + FOLDER_PATH)
+        repo.index.commit('New Photo')
+        print('made the commit')
+        origin.push()
+        print('pushed changes')
+    except:
+        print('Couldn\'t upload to git')
+
+
+def img_gen(name):
+    """
+    This function is complete. Generates a new image name.
+
+    Parameters:
+        name (str): your name ex. MasonM
+    """
+    t = time.strftime("_%H%M%S")
+    imgname = (f'{REPO_PATH}/{FOLDER_PATH}/{name}{t}.jpg')
+    return imgname
+
+def take_photo():
+    """
+    This function is NOT complete. Takes a photo when the FlatSat is shaken.
+    Replace psuedocode with your own code.
+    """
+    camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+    while True:
+        accelx, accely, accelz = accel_gyro.acceleration
+
+        #CHECKS IF READINGS ARE ABOVE THRESHOLD
+            #PAUSE
+            #name = ""     #First Name, Last Initial  ex. MasonM
+            #TAKE PHOTO
+            #PUSH PHOTO TO GITHUB
+        
+        #PAUSE
+        if accelx > THRESHOLD or accely > THRESHOLD or accelz > THRESHOLD:
+            picam2.configure(camera_config)
+            picam2.start_preview(Preview.QTGL)
+            picam2.start()
+            time.sleep(2)
+            picam2.capture_file("test.jpg")
+            git_push()
+            
+        
+
+
+def main():
+    take_photo()
+
+
+if __name__ == '__main__':
+    main()
