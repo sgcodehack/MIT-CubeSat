712501d9f4f3e7fffddf081a4afde561bc3aa719
sgcodehack <114374534+sgcodehack@users.noreply.github.com>
2024-02-21 18:36:30 -0800

    Add files via upload

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -19,6 +19,7 @@ import board
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
 from adafruit_lis3mdl import LIS3MDL
 from git import Repo
+from git import subprocess
 from picamera2 import Picamera2
 
 #VARIABLES
